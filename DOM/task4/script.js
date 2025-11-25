4d9279d0ca018f8ffdb86c93579cda31156651cc
laxmanalashetti <alashettilaxman@gmail.com>
2025-11-25 21:00:42 +0530

    task4 created Download % bar animation using setInterval and setTimeout

---
+++ DOM/task4/script.js
@@ -0,0 +1,23 @@
+let inner = document.querySelector('.inner');
+let h2 = document.querySelector('h2');
+let button = document.querySelector('button');
+
+ let grow = 0;
+button.addEventListener('click', function() {
+  
+  let interval = setInterval(function() {
+            grow++;
+            h2.textContent = grow + "%";
+            console.log(grow);
+            inner.style.width = grow + "%";
+} ,100);
+
+ setTimeout(() => {
+    
+    clearInterval(interval);
+    button.textContent = "Downloaded";
+    button.style.backgroundColor = "green";
+
+ }, 10000);
+});
+
